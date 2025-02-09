0c364d6b2bdd22a1aec818ed8c61a2e853fcf8c8
Mamed766 <mehemmeddemirli57@gmail.com>
2025-02-09 20:45:18 +0400

    light and dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold">Welcome to the course!</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="font-barlow">Welcome to the course!</h1>
+      <Button variant={"destructive"}>Click ulan</Button>
     </div>
   );
 }
