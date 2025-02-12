6a7dcdc96e4e481e2d9d76488d4f80860db1fcc8
Mamed766 <mehemmeddemirli57@gmail.com>
2025-02-12 19:26:38 +0400

    configuring admin navigation

---
+++ src/components/dashboard/sidebar/nav-admin.tsx
@@ -0,0 +1,10 @@
+import { DashboardSidebarMenuInterface } from "@/lib/types";
+import React from "react";
+
+export default function SidebarNavAdmin({
+  menuLinks,
+}: {
+  menuLinks: DashboardSidebarMenuInterface[];
+}) {
+  return <div></div>;
+}
