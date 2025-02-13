1cc25666fbe5b53fb8b4b9995a1ee87c824a1cdd
Mamed766 <mehemmeddemirli57@gmail.com>
2025-02-13 20:48:03 +0400

    updated

--- src/components/dashboard/forms/category-details.tsx
+++ src/components/dashboard/forms/category-details.tsx
@@ -1,13 +1,159 @@
-// Prisma model
+"use client";
+import { FC, useEffect } from "react";
 import { Category } from "@prisma/client";
-import { FC } from "react";
+import * as z from "zod";
+import { ControllerRenderProps, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CategoryFormSchema } from "@/lib/schemas";
+import { AlertDialog } from "@/components/ui/alert-dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 
 interface CategoryDetailsProps {
   data?: Category;
 }
 
-const CategoryDetails: FC<CategoryDetailsProps> = () => {
-  return <div></div>;
+const CategoryDetails: FC<CategoryDetailsProps> = ({ data }) => {
+  const defaultValues = {
+    name: data?.name || "",
+    image: data?.image ? [{ url: data.image }] : [],
+    url: data?.url || "",
+    featured: data?.featured ?? false,
+  };
+
+  const form = useForm<z.infer<typeof CategoryFormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(CategoryFormSchema),
+    defaultValues,
+  });
+
+  useEffect(() => {
+    if (data) {
+      form.reset({
+        name: data?.name || "",
+        image: data?.image ? [{ url: data.image }] : [],
+        url: data?.url || "",
+        featured: data?.featured ?? false,
+      });
+    }
+  }, [data, form]);
+
+  const isLoading = form.formState.isSubmitting;
+
+  const handleSubmit = async (values: z.infer<typeof CategoryFormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <AlertDialog>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Category</CardTitle>
+          <CardDescription>
+            {data?.id
+              ? `Update ${data.name} category information.`
+              : "Let's create a category. You can edit it later from the categories table or category page."}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="space-y-4"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof CategoryFormSchema>,
+                    "name"
+                  >;
+                }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Category name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="url"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof CategoryFormSchema>,
+                    "url"
+                  >;
+                }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Category URL</FormLabel>
+                    <FormControl>
+                      <Input placeholder="/category-url" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="featured"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormLabel>Featured</FormLabel>
+                      <FormDescription>
+                        This Category will appear on the home page.
+                      </FormDescription>
+                    </div>
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" disabled={isLoading}>
+                {isLoading
+                  ? "loading..."
+                  : data?.id
+                  ? "Save category information"
+                  : "Create category"}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </AlertDialog>
+  );
 };
 
 export default CategoryDetails;
